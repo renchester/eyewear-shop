23636df4b26fc185946d82ab2a045434813ed499
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-21 17:23:10 +0800

    Initialize header component and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,61 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/products">Shop</Link>
-        <Link to="/about">About</Link>
-      </ul>
-    </div>
+    <header className="header">
+      <div className="header__main">
+        <div className="header__main--extra-message">
+          Free shipping on orders above 10000
+        </div>
+
+        <Link to="/" className="header__logo">
+          <h1 className="header__logo-main">SAVANT</h1>
+          <h2 className="header__logo-sub">EYEWEAR</h2>
+        </Link>
+        <div className="header__icons-wrapper">
+          <div className="header__icon icon__search material-symbols-outlined">
+            search
+          </div>
+          <div className="header__icon icon__shopping-bag material-symbols-outlined">
+            shopping_bag
+          </div>
+        </div>
+      </div>
+
+      <nav className="nav">
+        <ul className="nav__links">
+          <NavLink
+            to="/products"
+            className="nav__link
+          "
+          >
+            Collection
+          </NavLink>
+          <NavLink
+            to="/products/frames"
+            className="nav__link
+          "
+          >
+            Eyeglasses
+          </NavLink>
+          <NavLink
+            to="/products/sunnies"
+            className="nav__link
+          "
+          >
+            Sunglasses
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className="nav__link
+          "
+          >
+            About us
+          </NavLink>
+        </ul>
+      </nav>
+    </header>
   );
 }
 export default Header;
