c5482c2d48e7e72fd3b9f5ae5dda50571105739e
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-25 21:45:31 +0800

    Allow image wrapper component to take class prop

--- src/components/ImageWrapper.jsx
+++ src/components/ImageWrapper.jsx
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types';
 
 function ImageWrapper(props) {
-  const { children } = props;
+  const { children, className } = props;
 
-  return <div className="img-wrapper">{children}</div>;
+  return <div className={`img-wrapper ${className}`}>{children}</div>;
 }
 
 ImageWrapper.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
+
+ImageWrapper.defaultProps = {
+  className: '',
+};
+
 export default ImageWrapper;
