c3b86b8743624f12fd0310bd8bb0dcaa015f61db
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-25 22:50:20 +0800

    Update product page sections and styles

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -114,142 +114,172 @@ function ProductPage() {
   };
 
   return (
-    <main className="pd-pg">
-      <section className="pd-pg__img--container">
-        <nav className="pd-pg__nav">
-          <Link to="/" className="pd-pg__nav-link">
-            HOME
-          </Link>{' '}
-          /
-          {product.type === 'sunnies' ? (
-            <Link to="/products/sunglasses" className="pd-pg__nav-link">
-              Sunglasses
-            </Link>
-          ) : (
-            <Link to="/products/eyeglasses" className="pd-pg__nav-link">
-              Eyeglasses
-            </Link>
-          )}{' '}
-          /<span className="pd-pg__nav-link">{product.brand}</span>
-        </nav>
-        <div className="pd-pg__img--wrapper">
-          <ImageWrapper>
-            {activeImg === 1 ? (
-              <img
-                src={product.images.main}
-                alt={`${product.title} front view`}
-                className="pd-pg__active-img"
-              />
+    <>
+      <main className="pd-pg">
+        <section className="pd-pg__img--container">
+          <nav className="pd-pg__nav">
+            <Link to="/" className="pd-pg__nav-link">
+              HOME
+            </Link>{' '}
+            /
+            {product.type === 'sunnies' ? (
+              <Link to="/products/sunglasses" className="pd-pg__nav-link">
+                Sunglasses
+              </Link>
             ) : (
-              <img
-                src={product.images.side}
-                alt={`${product.title} side view`}
-                className="pd-pg__active-img"
-              />
-            )}
-          </ImageWrapper>
-          <div className="pd-pg__img-thumbs">
-            <button
-              className={`pd-pg__img-thumbnail--wrapper ${
-                activeImg === 1 ? 'active' : ''
-              }`}
-              type="button"
-              onClick={() => setActiveImg(1)}
-            >
-              <img
-                src={product.images.main}
-                alt={`${product.title} front view`}
-                className="pd-pg__img-thumbnail"
-              />
-            </button>
-            <button
-              className={`pd-pg__img-thumbnail--wrapper ${
-                activeImg === 2 ? 'active' : ''
-              }`}
-              type="button"
-              onClick={() => setActiveImg(2)}
-            >
-              <img
-                src={product.images.side}
-                alt={`${product.title} side view`}
-                className="pd-pg__img-thumbnail"
-              />
-            </button>
-          </div>
-        </div>
-      </section>
-      <section className="pd-pg__details--container">
-        <h1 className="pd-pg__title">{product.title}</h1>
-        <div className="pd-pg__price--wrapper">
-          <h3 className="pd-pg__price">{product.price}</h3>
-          {product.onSale && (
-            <h3 className="pd-pg__og-price">{product.originalPrice}</h3>
-          )}
-        </div>
-        <div className="pd-pg__details">
-          <p className="pd-pg__description">{product.description}</p>
-          <ul className="pd-pg__tech-details">
-            {product.technicalDetails.map((td) => (
-              <li className="pd-pg__tech-detail" key={td}>
-                {td}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="pd-pg__opt-details">
-          {product.color && (
-            <div className="pd-pg__color--wrapper">
-              <span className=" pd-pg__opt-label pd-pg__color-label">
-                Color:{' '}
-              </span>
-              <span className="pd-pg__color">{product.color}</span>
-            </div>
-          )}
-          {product.quantity > 0 ? (
-            <form onSubmit={addToCart} noValidate>
-              <label
-                htmlFor="pd-quantity"
-                className=" pd-pg__opt-label pd-pg__quantity-label"
-              >
-                Quantity:
-                <input
-                  type="number"
-                  className="pd-pg__quantity-input"
-                  name="pd-quantity"
-                  id="pd-quantity"
-                  max={product.quantity}
-                  min={1}
-                  onChange={handleDesiredQty}
-                  value={desiredQty}
+              <Link to="/products/eyeglasses" className="pd-pg__nav-link">
+                Eyeglasses
+              </Link>
+            )}{' '}
+            /<span className="pd-pg__nav-link">{product.brand}</span>
+          </nav>
+          <div className="pd-pg__img--wrapper">
+            <ImageWrapper>
+              {activeImg === 1 ? (
+                <img
+                  src={product.images.main}
+                  alt={`${product.title} front view`}
+                  className="pd-pg__active-img"
                 />
-              </label>
+              ) : (
+                <img
+                  src={product.images.side}
+                  alt={`${product.title} side view`}
+                  className="pd-pg__active-img"
+                />
+              )}
+            </ImageWrapper>
+            <div className="pd-pg__img-thumbs">
               <button
-                className="pd-pg__btn-cart pd-pg__btn-add-cart"
-                type="submit"
+                className={`pd-pg__img-thumbnail--wrapper ${
+                  activeImg === 1 ? 'active' : ''
+                }`}
+                type="button"
+                onClick={() => setActiveImg(1)}
               >
-                Add to Cart
+                <img
+                  src={product.images.main}
+                  alt={`${product.title} front view`}
+                  className="pd-pg__img-thumbnail"
+                />
               </button>
-              {itemIsInCart && (
-                <Link
-                  to="/cart"
-                  className="pd-pg__btn-cart pd-pg__btn-checkout"
-                >
-                  Buy it now
-                </Link>
-              )}
-
-              {errorMessage && (
-                <span className="pd-pg__error">{errorMessage}</span>
-              )}
-            </form>
-          ) : (
-            <div className="pd-pg__quantity-sold-out">
-              Item is currently sold out. Keep posted for updates!
+              <button
+                className={`pd-pg__img-thumbnail--wrapper ${
+                  activeImg === 2 ? 'active' : ''
+                }`}
+                type="button"
+                onClick={() => setActiveImg(2)}
+              >
+                <img
+                  src={product.images.side}
+                  alt={`${product.title} side view`}
+                  className="pd-pg__img-thumbnail"
+                />
+              </button>
             </div>
-          )}
-        </div>
+          </div>
+        </section>
+        <section className="pd-pg__details--container">
+          <h1 className="pd-pg__title">{product.title}</h1>
+          <div className="pd-pg__price--wrapper">
+            <h3 className="pd-pg__price">{product.price}</h3>
+            {product.onSale && (
+              <h3 className="pd-pg__og-price">{product.originalPrice}</h3>
+            )}
+          </div>
+          <div className="pd-pg__details">
+            <p className="pd-pg__description">{product.description}</p>
+            <ul className="pd-pg__tech-details">
+              {product.technicalDetails.map((td) => (
+                <li className="pd-pg__tech-detail" key={td}>
+                  {td}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="pd-pg__opt-details">
+            {product.color && (
+              <div className="pd-pg__color--wrapper">
+                <span className=" pd-pg__opt-label pd-pg__color-label">
+                  Color:{' '}
+                </span>
+                <span className="pd-pg__color">{product.color}</span>
+              </div>
+            )}
+            {product.quantity > 0 ? (
+              <form onSubmit={addToCart} noValidate>
+                <label
+                  htmlFor="pd-quantity"
+                  className=" pd-pg__opt-label pd-pg__quantity-label"
+                >
+                  Quantity:
+                  <input
+                    type="number"
+                    className="pd-pg__quantity-input"
+                    name="pd-quantity"
+                    id="pd-quantity"
+                    max={product.quantity}
+                    min={1}
+                    onChange={handleDesiredQty}
+                    value={desiredQty}
+                  />
+                </label>
+                <button
+                  className="pd-pg__btn-cart pd-pg__btn-add-cart"
+                  type="submit"
+                >
+                  Add to Cart
+                </button>
+                {itemIsInCart && (
+                  <Link
+                    to="/cart"
+                    className="pd-pg__btn-cart pd-pg__btn-checkout"
+                  >
+                    Buy it now
+                  </Link>
+                )}
+                {errorMessage && (
+                  <span className="pd-pg__error">{errorMessage}</span>
+                )}
+              </form>
+            ) : (
+              <div className="pd-pg__quantity-sold-out">
+                Item is currently sold out. Keep posted for updates!
+              </div>
+            )}
+          </div>
+        </section>
+      </main>
+      <section className="pd-lens">
+        <h2 className="pd-lens__title">Purchase Prescription Lenses</h2>
+        <ul className="pd-lens__steps">
+          <li className="pd-lens__step">
+            <span className="pd-lens__count">1</span>
+            <p className="pd-lens__instruction">
+              Purchase the frame (Optical or Sunglasses) you wish to have a
+              prescription. Skip this step if you wish to add a prescription to
+              a frame you already have.
+            </p>
+          </li>
+          <li className="pd-lens__step">
+            <span className="pd-lens__count">2</span>
+            <p className="pd-lens__instruction">
+              Schedule an appointment through our website or contact us through
+              our telephone number. A SAVANT Eyewear representative will reach
+              out to confirm your appointment.
+            </p>
+          </li>
+          <li className="pd-lens__step">
+            <span className="pd-lens__count">3</span>
+            <p className="pd-lens__instruction">
+              Attend your scheduled appointment and have your order for
+              prescription lenses finalized. Once finalized, your frame will be
+              delivered to your address.
+            </p>
+          </li>
+        </ul>
       </section>
-    </main>
+    </>
   );
 }
 export default ProductPage;
